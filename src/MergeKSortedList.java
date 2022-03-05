69e5c2055fc44fb458c7611dffc4ff0df628c407
mritunjoy <mritunjoy@revesoft.com>
2022-03-06 01:50:36 +0600

    leetcode

--- src/MergeKSortedList.java
+++ src/MergeKSortedList.java
@@ -1,36 +1,36 @@
 import java.util.*;
 
-class ListNode {
+class ListNode1 {
     int val;
-    ListNode next;
+    ListNode1 next;
 
-    ListNode() {
+    ListNode1() {
     }
 
-    ListNode(int val) {
+    ListNode1(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    ListNode1(int val, ListNode1 next) {
         this.val = val;
         this.next = next;
     }
 }
 
 public class MergeKSortedList {
-    public ListNode mergeKLists(ListNode[] lists) {
-        ListNode head = new ListNode(0);
+    public ListNode1 mergeKLists(ListNode1[] lists) {
+        ListNode1 head = new ListNode1(0);
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        for (ListNode l :lists) {
+        for (ListNode1 l :lists) {
             while (l != null) {
                 pq.add(l.val);
                 l = l.next;
             }
 
         }
-        ListNode node = head;
+        ListNode1 node = head;
         while (!pq.isEmpty()) {
-            node.next = new ListNode((int) pq.remove());
+            node.next = new ListNode1((int) pq.remove());
             node = node.next;
         }
         return head.next;
