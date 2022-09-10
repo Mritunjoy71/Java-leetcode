1d4a1737242904f08bbbe50fe9cb1fdd34eb78ad
mritunjoy <mritunjoy@revesoft.com>
2022-09-11 01:13:34 +0600

    add all

---
+++ src/RansomeNote/Solution.java
@@ -0,0 +1,35 @@
+package RansomeNote;
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            char ch = magazine.charAt(i);
+            int currentCount = hashMap.getOrDefault(ch, 0);
+            hashMap.put(ch, currentCount + 1);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            int currentCount = hashMap.getOrDefault(ch, 0);
+            if (currentCount == 0)
+                return false;
+            hashMap.put(ch, currentCount - 1);
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String ransomNote = "aaa", magazine = "aab";
+        Solution solution = new Solution();
+        System.out.println("Result: " + solution.canConstruct(ransomNote, magazine));
+    }
+
+}
+
+
+
